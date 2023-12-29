bbefc1387efd85880cb2490dbc4f48f85c289763
tomvik <lugo.lbmi@gmail.com>
2023-12-29 15:59:14 -0800

    Initial experiment

---
+++ Python/src/Motor.py
@@ -0,0 +1,31 @@
+from RPi.gpio_proxy import *
+from time import sleep
+
+KPIN1 = 20
+KPIN2 = 21
+KPWM1 = 16
+
+GPIO = GPIOProxy()
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(KPIN1, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(KPIN2, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(KPWM1, GPIO.OUT, initial = GPIO.HIGH)
+
+pwm_ENA = GPIO.PWM(KPWM1, 2000)
+pwm_ENA.start(0)
+
+GPIO.output(KPIN1, GPIO.HIGH)
+GPIO.output(KPIN2, GPIO.LOW)
+pwm_ENA.ChangeDutyCycle(50)
+sleep(2)
+GPIO.output(KPIN1, GPIO.LOW)
+GPIO.output(KPIN2, GPIO.LOW)
+pwm_ENA.ChangeDutyCycle(0)
+sleep(2)
+GPIO.output(KPIN1, GPIO.LOW)
+GPIO.output(KPIN2, GPIO.HIGH)
+pwm_ENA.ChangeDutyCycle(0)
---
+++ Python/src/RPi/gpio_proxy.py
@@ -0,0 +1,65 @@
+_rpiLoaded = True
+
+try:
+    import RPi.GPIO as GPIO
+except:
+    _rpiLoaded = False
+
+class PWMProxy():
+    def start(*args, **kwargs):
+        print('start')
+
+    def ChangeDutyCycle(*args, **kwargs):
+        print('ChangeDutyCycle')
+
+class GPIOProxy():
+    BCM = GPIO.BCM if _rpiLoaded else 'BCM'
+
+    HIGH = GPIO.HIGH if _rpiLoaded else 'HIGH'
+    LOW = GPIO.LOW if _rpiLoaded else 'LOW'
+
+    IN = GPIO.IN if _rpiLoaded else 'IN'
+    OUT = GPIO.OUT if _rpiLoaded else 'OUT'
+
+    FALLING = GPIO.FALLING if _rpiLoaded else 'FALLING'
+    RISING = GPIO.RISING if _rpiLoaded else 'RISING'
+    BOTH = GPIO.BOTH if _rpiLoaded else 'BOTH'
+
+    PUD_UP = GPIO.PUD_UP if _rpiLoaded else 'PUD_UP'
+    PUD_DOWN = GPIO.PUD_DOWN if _rpiLoaded else 'PUD_DOWN'
+
+    def setmode(*args, **kwargs):
+        if _rpiLoaded:
+            GPIO.setmode(*args, **kwargs)
+        else:
+            pass
+
+    def setwarnings(*args, **kwargs):
+        if _rpiLoaded:
+            GPIO.setwarnings(*args, **kwargs)
+        else:
+            pass
+
+    def setup(*args, **kwargs):
+        if _rpiLoaded:
+            GPIO.setup(*args, **kwargs)
+        else:
+            pass
+
+    def output(*args, **kwargs):
+        if _rpiLoaded:
+            GPIO.output(*args, **kwargs)
+        else:
+            pass
+
+    def add_event_detect(*args, **kwargs):
+        if _rpiLoaded:
+            GPIO.add_event_detect(*args, **kwargs)
+        else:
+            pass
+
+    def PWM(*args, **kwargs):
+        if _rpiLoaded:
+            return GPIO.PWM(*args, **kwargs)
+        else:
+            return PWMProxy()
